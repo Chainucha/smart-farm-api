2a7bb38f9546693cfd02db63f520dc0172a3d91f
Chainucha <chainucha.work@gmail.com>
2025-04-29 23:51:48 +0700

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,27 +2,32 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
+from google.cloud.sql.connector import Connector
 
 load_dotenv()
-
-required_vars = ["DB_USER", "DB_PASSWORD", "DB_NAME"]
-for var in required_vars:
-    if not os.environ.get(var):
-        raise ValueError(f"Environment variable {var} is missing")
 
 DB_USER = os.environ.get("DB_USER")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
 DB_NAME = os.environ.get("DB_NAME")
-DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT", "3306")
-DB_SOCKET = os.environ.get("DB_SOCKET")  # Optional for Cloud SQL socket
+DB_INSTANCE = os.environ.get("DB_INSTANCE")
 
-if DB_SOCKET:
-    DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@/{DB_NAME}?unix_socket={DB_SOCKET}"
-else:
-    DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+required_vars = ["DB_USER", "DB_PASSWORD", "DB_NAME","DB_INSTANCE"]
+for var in required_vars:
+    if not os.environ.get(var):
+        raise ValueError(f"Environment variable {var} is missing")
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+connector = Connector()
+
+# initialize SQLAlchemy connection pool with Connector
+engine = create_engine(
+    "mysql+pymysql://",
+    creator=lambda: connector.connect(
+        DB_INSTANCE,
+        "pymysql",
+        user=DB_USER,
+        password=DB_PASSWORD,
+        db=DB_NAME
+    ), pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
