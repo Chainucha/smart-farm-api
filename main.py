b60342be99fbdae970105f7c28e139e3e0420bfb
Chainucha <chainucha.work@gmail.com>
2025-04-30 01:34:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -50,4 +50,5 @@ def get_sensor(sensor_type: str, db: Session = Depends(get_db)):
     return sensor
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8000))  # fallback if PORT is not set
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
